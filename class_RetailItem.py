44ec6f3f1cb73d48602a6327d164f4cc173b2182
Jacobloseycoder <loseyjacob1@gmail.com>
2025-04-24 13:58:12 -0500

    Update class_RetailItem.py

--- class_RetailItem.py
+++ class_RetailItem.py
@@ -3,29 +3,57 @@ class retail:
         self.item = item
         self.unit = unit
         self.price = price
-    def desplay(self):
-        #opens the file
-        name_list = []
-        bill = 0
-        itemlist = open('item.txt', r)
-        for line in itemlist:
-            # Split by the two dot thing
-            item, unit, price = line.strip().split(',')
-            #adds the name of the item to a list
-            name_list.append(item)
-            #adds to the class with the names from the list 
-            name_list[bill] = retail(item, unit, price)
-            bill = bill + 1
-        #shows the items from class
-#         billmax = bill
-#         bill = 0
-#         print('name___units___price')
-#         while bill != billmax:
-#             pass
-        #close file
-    def save(self):
-        pass
-        #saves thing to item.txt
-    def end(self):
-        pass
-        #ends/gos back to main
+    def printer(self):
+        print(self.item, self.unit, self.price)
+def add():
+    toadd = []
+    #makes a list of things to be added
+    new_item = input('enter the items name')
+    new_unit = input('enter the number of the item')
+    new_price = input('enter the items price')
+    if new_unit.isdigit() == True:
+        if new_unit.isdigit() == True:
+            if new_item.isalpha() == True:
+                toadd.append(new_item)
+                toadd.append(new_unit)
+                toadd.append(new_price)
+                agion = input('do you want to add another? y/n :')
+                if agion == 'y':
+                    add()
+                elif agion == 'n':
+                    menu()
+                else:
+                    print('not a option')
+                    menu()
+            else:
+                print('not a word')
+                add()
+        else:
+            print('not a number')
+            add()
+    else:
+        print('not a number')
+        add()
+def save():
+    pass
+    #saves thing to item.txt
+
+def end():
+    #ends/gos back to main
+    print('returning to main menu')
+    main()
+
+def display():
+    #opens the file
+    itemlist = open('items.txt', 'r')
+    print('name units price')
+    #loops
+    for line in itemlist:
+        # Split by the two dot thing
+        item, unit, price = line.strip().split(',')
+        #makes the class
+        jimmy = retail(item, unit, price)
+        #prints the list
+        jimmy.printer()
+    #close file
+    itemlist.close()
