eadce56a22406b122c7beada1812e7df526ed361
jacob losey <loseyjacob1@gmail.com>
2025-04-25 08:27:09 -0500

    Update main.py
    Signed-off-by: jacob losey <loseyjacob1@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+def main():
+  print('1. retail system')
+  print('2. cash register system')
+  print('3. end')
+  gig = input('enter here:')
+  if gig == '1':
+    retailpassword()
+  elif gig == '2':
+    crmain()
+  elif gigi == '3':
+    print('ending...')
+  else:
+    print('not a option')
 
+def retailpassword():
+  pasword = '12345'
+  print('please enter the admin pasword')
+  bobo = input('enter here:')
+  if bobo == pasword:
+    rtmain()
+  else:
+    print('wrong password retuning to main menu')
+    main()
+
+def rtmain():
+  print('1.')
+  print('1.')
+  print('1.')
+  print('1.')
