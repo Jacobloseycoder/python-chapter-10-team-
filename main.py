05cfc7884d29690f7b692e3555b99e03caf9bcd8
jacob losey <loseyjacob1@gmail.com>
2025-04-25 13:28:28 -0500

    Update main.py
    Signed-off-by: jacob losey <loseyjacob1@gmail.com>

--- main.py
+++ main.py
@@ -23,7 +23,101 @@ def retailpassword():
     main()
 
 def rtmain():
-  print('1.')
-  print('1.')
-  print('1.')
-  print('1.')
+  print('1. desplay')
+  print('2. add')
+  print('3. save')
+  print('4. exit')
+  koko = input('enter here:')
+  if koko == '1':
+    display()
+  elif koko == '2':
+    add()
+  elif koko == '3':
+    save()
+  elif koko == '4':
+    end()
+  else:
+    print('not a option')
+    rtmain()
+
+class retail:
+    def __init__(self, item, unit, price):
+        self.item = item
+        self.unit = unit
+        self.price = price
+    def printer(self):
+        print(self.item, self.unit, self.price)
+
+toadd = []
+def add():
+    #makes a list of things to be added
+    #asks for the items needed parts
+    new_item = input('enter the items name')
+    new_unit = input('enter the number of the item')
+    new_price = input('enter the items price')
+    #checks if name in only leters and price and units is only numbers
+    if new_unit.isdigit() == True and int(new_unit) > 0:
+        if new_price.isdigit() == True and int(new_price) > 0:
+            if new_item.isalpha() == True:
+                #adds the item
+                toadd.append(new_item)
+                toadd.append(new_unit)
+                toadd.append(new_price)
+                #let user add another
+                agion = input('do you want to add another? y/n :')
+                #checks if they say yes or no
+                if agion == 'y' or agion == 'Y':
+                    add()
+                elif agion == 'n' or agion == 'N':
+                    rtmain()
+                else:
+                    print('not a option')
+                    rtmain()
+            else:
+                print('not a word')
+                add()
+        else:
+            print('not a number')
+            add()
+    else:
+        print('not a number')
+        add()
+def save():
+    #saves thing to item.txt
+    popopopo = []
+    #opens file
+    itemlist = open('items.txt', 'a')
+    while toadd != popopopo:
+        #adds the items
+        itemlist.write("\n")
+        itemlist.write(toadd[0])
+        itemlist.write(',')
+        itemlist.write(toadd[1])
+        itemlist.write(',')
+        itemlist.write(toadd[2])
+        #deleats the items
+        del toadd[0]
+        del toadd[0]
+        del toadd[0]
+    rtmain()
+
+def end():
+    #ends/gos back to main
+    print('returning to main menu')
+    main()
+
+def display():
+    #opens the file
+    itemlist = open('items.txt', 'r')
+    print('name units price')
+    #loops
+    for line in itemlist:
+        # Split by the two dot thing
+        item, unit, price = line.strip().split(',')
+        #makes the class
+        jimmy = retail(item, unit, price)
+        #prints the list
+        jimmy.printer()
+    #close file
+    itemlist.close()
+  rtmain()
